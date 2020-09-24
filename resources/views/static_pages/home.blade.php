7c07e544d7c7bf45df637cf5183b6ec36ce23793
deerjia <544375932@qq.com>
2020-09-24 06:20:31 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.default')
+
 @section('title', '主页')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+      <h1>Hello Laravel</h1>
+      <p class="lead">
+        现在看到的是 <a href="">Laravel 入门教程</a> 的主页
+      </p>
+      <p>
+        一切，从这儿开始
+      </p>
+      <p>
+        <a href="#" class="btn btn-lg btn-success" role="button">现在注册</a>
+      </p>
+    </div>
 @stop
